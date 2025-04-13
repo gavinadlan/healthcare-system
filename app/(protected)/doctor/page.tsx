c506ef3838b9c446de4bbc1cbf634604ff592bfa
giep213 <gavinadlan@gmail.com>
2025-04-14 01:14:36 +0700

    Initial Commit

--- app/(protected)/doctor/page.tsx
+++ app/(protected)/doctor/page.tsx
@@ -1,6 +1,15 @@
+import { checkRole, getRole } from "@/utils/roles";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const DoctorDashboard = () => {
+const DoctorDashboard = async () => {
+  const isDoctor = await checkRole("DOCTOR");
+  const role = await getRole();
+
+  if (!isDoctor) {
+    redirect(`/${role}`);
+  }
+
   return <div>DoctorDashboard</div>;
 };
 
--- utils/roles.ts
+++ utils/roles.ts
@@ -3,5 +3,14 @@ import { auth } from "@clerk/nextjs/server";
 
 export const checkRole = async (role: Roles) => {
   const { sessionClaims } = await auth();
-  return sessionClaims?.metadata.role === role;
+
+  return sessionClaims?.metadata.role === role.toLowerCase();
+};
+
+export const getRole = async () => {
+  const { sessionClaims } = await auth();
+
+  const role = sessionClaims?.metadata.role!.toLowerCase();
+
+  return role;
 };
